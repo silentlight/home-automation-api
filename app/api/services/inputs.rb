f89a57748a5fef124b3e4638c199dedfae828a4d
Bartosz Hejman <bartoszhejman@gmail.com>
2015-01-17 00:41:42 +0100

    Edit led status endpoint

--- app/api/services/inputs.rb
+++ app/api/services/inputs.rb
@@ -2,16 +2,15 @@ class Services::Inputs < Grape::API
   resources :inputs do
 
     params do
-      optional :state,  type: Integer
+      requires :state,  type: Integer
     end
-    get 'led' do
-      if params[:state].present?
-        action = (params[:state] == 1) ? 'led-on' : 'led-off'
-      else
-        action = 'led-status'
-      end
+    get 'led/:state' do
+      action = (params[:state] == 1) ? 'led-on' : 'led-off'
       JSON.parse(RestClient.get("#{ENV['ARDUINO_URL']}/#{action}"))
     end
 
+    get 'led-status' do
+      JSON.parse(RestClient.get("#{ENV['ARDUINO_URL']}/led-status"))
+    end
   end
 end
