3a3869851f696228a50c70d4197d73baa732b4aa
Bartosz Hejman <bartoszhejman@gmail.com>
2015-01-17 00:24:15 +0100

    Add led status check

--- app/api/services/inputs.rb
+++ app/api/services/inputs.rb
@@ -2,10 +2,14 @@ class Services::Inputs < Grape::API
   resources :inputs do
 
     params do
-      requires :state,  type: Integer
+      options :state,  type: Integer
     end
-    get 'led/:state' do
-      action = (params[:state] == 1) ? 'led-on' : 'led-off'
+    get 'led' do
+      if params[:state].present?
+        action = (params[:state] == 1) ? 'led-on' : 'led-off'
+      else
+        action = 'led-status'
+      end
       JSON.parse(RestClient.get("#{ENV['ARDUINO_URL']}/#{action}"))
     end
 
